f14631efa7359f6cee1327339925d486b00b64bc
jaztitch <alterjaz@gmail.com>
2025-07-18 14:32:43 -0500

    Create Conditions_Loops.cpp

---
+++ CPP_Basics_Level_3/Conditions_Loops.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	cout << boolalpha;
+	
+	double temperature = 25.0;
+	
+	cout << ( (temperature >= 20.0) && (temperature < 30.0)) << endl;
+	
+	return 0;
+}
